e3baf7d54d50df38497ad8d8bfaa06e5d13abd27
iberis2 <snow9402@gmail.com>
2023-04-08 17:34:51 +0900

    nav 컴포넌츠에 LinkNav 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Container from "./Container";
 import UserMenu from "./UserMenu";
 import logoImg from "../assets/logo.svg";
 import styles from "./Nav.module.css";
+
+function getLinkStyle({ isActive }) {
+  return {
+    textDecoration: isActive ? "underline" : undefined,
+  };
+}
 
 function Nav() {
   return (
@@ -12,11 +18,16 @@ function Nav() {
           <img src={logoImg} alt="Codethat Logo" />
         </Link>
         <ul className={styles.menu}>
-          <Link to="/courses">
-            <li>카탈로그</li>
-          </Link>
           <li>
-            <Link to="/questions">커뮤니티</Link>
+            <NavLink to="/courses" style={getLinkStyle}>
+              카탈로그
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/questions" style={getLinkStyle}>
+              커뮤니티
+            </NavLink>
           </li>
           <li>
             <UserMenu />
